837408c87680a917a523254a56a466fd19b324ab
genenartey3 <nartey@college.harvard.edu>
2019-01-09 12:56:00 -0500

    Html Site

--- 2_PM/placeinfo.js
+++ 2_PM/placeinfo.js
@@ -2,3 +2,14 @@
 / returns the following string, formatted with the appropriate values:
 
 'INPUTTED_PLACE is located at coordinates RETURNED_COORDINATES, at the address RETURNED_ADDRESS.' */
+const client  = require('./google_api')
+
+// fetch('https://maps.googleapis.com/maps/api/place/details/json?&key=AIzaSyBXGSsSDuDecaYMwfAbE54T0VzM5Sm6cbo&placeid=ChIJ0T7l6EJ344kRmlihQjBlHvk')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+
+client.findPlace({ input: process.argv[2] , inputtype: 'textquery' })
+.asPromise()
+then((response) => { 
+    const placeid = respnse.json.candidates[0].place_id
+})
--- 3_AM/index.js
+++ 3_AM/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const path = require('path')
 const app = express();
+app.use(express.static('public'))
 
 app.use(express.static(__dirname + "/View"));
 app.use(express.static(__dirname + "/Script"));
@@ -7,6 +9,10 @@ app.use(express.static(__dirname + "/Script"));
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
+
+app.get('/huncho', (req, res) => {
+    res.sendFile(path.join(__dirname + '/huncho.html'))
+})
 
 app.listen(3000);
 console.log("Running at port 3000");
